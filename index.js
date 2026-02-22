65f6e4b50c06d7b7e4c2dd5f6ed476edb346809a
joserinaldi958-maker <joserinaldi958@gmail.com>
2026-02-22 17:08:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
 const express = require("express");
-const mercadopago = require("mercadopago");
+const app  = express();
 
-const app = express();
-app.use(express.json());
-
-mercadopago.configure({
-  access_token: process.env.MP_ACCESS_TOKEN
-});
-
-// Ruta de prueba
 app.get("/", (req, res) => {
-  res.send("Backend Mercado Pago Wallet funcionando OK");
-});
-
-// Crear pago
-app.post("/crear-pago", async (req, res) => {
-  try {
-    const preference = {
-      items: [
-        {
-          title: "Carga de saldo",
-          quantity: 1,
-          unit_price: 100
-        }
-      ]
-    };
-
-    const response = await mercadopago.preferences.create(preference);
-    res.json({ id: response.body.id });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error al crear pago" });
-  }
+  res.send("Backend Mercado Pago Funcionando");
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor activo en puerto", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(" Servidor corriendo en puerto " + PORT);
 });
-console.log("deploy test");
-
-console.log("deploy test");
